c8258d4e483043c9f81c30b9df442277fe29497b
wtkeqrf0 <matvey-sizov@mail.ru>
2023-09-17 19:11:28 +0300

    feat: implement `updateEnrichedFio` query

---
+++ api/update_enriched_fio.go
@@ -0,0 +1,46 @@
+package api
+
+import (
+	"context"
+	"github.com/go-playground/validator/v10"
+	"github.com/wtkeqrf0/restService/pkg/ent"
+	"github.com/wtkeqrf0/restService/postgres"
+)
+
+type UpdateEnrichedFIORequest struct {
+	EnrichedFio
+}
+
+type UpdateEnrichedFIOResponse struct {
+	User *User
+}
+
+func (r UpdateEnrichedFIORequest) Validate() error {
+	err := vr.Struct(r)
+	if err == nil {
+		return nil
+	}
+	errs := err.(validator.ValidationErrors)
+
+	return newValidationError(errs)
+}
+
+func (s *Server) UpdateEnrichedFIO(ctx context.Context, r UpdateEnrichedFIORequest) (UpdateEnrichedFIOResponse, error) {
+	user, err := s.ctrl.UpdateUser(ctx, postgres.UpdateEnrichedFIO(r.EnrichedFio))
+	if err != nil {
+		if _, ok := err.(*ent.NotFoundError); ok {
+			return UpdateEnrichedFIOResponse{}, newError(err, ErrorNotFound)
+		}
+		return UpdateEnrichedFIOResponse{}, newError(err, ErrorInternal)
+	}
+
+	return UpdateEnrichedFIOResponse{User: &User{
+		ID:         user.ID,
+		Name:       user.Name,
+		Surname:    user.Surname,
+		Patronymic: user.Patronymic,
+		Age:        user.Age,
+		Gender:     user.Gender,
+		Country:    user.Country,
+	}}, err
+}
