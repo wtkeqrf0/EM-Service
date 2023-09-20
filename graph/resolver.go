900accab82fcf71f0cae960a5f7d199a51a600a9
wtkeqrf0 <matvey-sizov@mail.ru>
2023-09-20 19:18:38 +0300

    feat: create custom `GraphQL` validator

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,6 +1,11 @@
 package graph
 
-import "github.com/wtkeqrf0/restService/api"
+import (
+	"context"
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/vektah/gqlparser/v2/gqlerror"
+	"github.com/wtkeqrf0/restService/api"
+)
 
 // This file will not be regenerated automatically.
 //
@@ -8,4 +13,22 @@ import "github.com/wtkeqrf0/restService/api"
 
 type Resolver struct {
 	*api.Server
+}
+
+func Validator(ctx context.Context, next graphql.Resolver) (any, error) {
+	if v, ok := graphql.GetFieldContext(ctx).Args["req"].(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			graphql.AddError(ctx, &gqlerror.Error{
+				Message: "Request is not valid",
+				Path:    graphql.GetPath(ctx),
+				Extensions: map[string]interface{}{
+					"code":  err.Error(),
+					"error": err,
+				},
+			})
+			return nil, nil
+		}
+	}
+
+	return next(ctx)
 }
